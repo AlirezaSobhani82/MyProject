dc15e29d52c066e9ba7166e1b8804b32247b60e0
Alireza Sobhani <108984123+AlirezaSobhani82@users.noreply.github.com>
2025-05-22 15:06:34 +0330

    Delete max number1.py

--- max number1.py
+++
@@ -1,8 +0,0 @@
-number = range(1,11)
-max = 0
-for each_number in number:
-    num = int(input("please insert the number{}:".format(each_number)))
-    if num > max:
-        max = num
-
-print("the maximum number is {}".format(max))
